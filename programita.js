14bb5806efb763270ed96cf3c6de8e7d69c99865
andres.garrido@ESTUDIO.LOCAL <andres.garrido@ESTUDIO.LOCAL>
2017-11-06 19:34:35 +0100

    paso 1

---
+++ programita.js
@@ -0,0 +1,18 @@
+"use strict"
+/**
+ * 
+ * @file Este archivo contiene ejemplos de funciones con "for"
+ * @author Andrés Garrido <andyyeha@gmail.com>
+ * @version 1.0.0
+ */
+
+ /**
+  * @description SumaArray Suma los valores de un array
+  * @param {Array} numeros Array de números
+  * @return {number}
+  */
+  function SumaArray (numeros) {
+      let suma=0;
+
+      return suma;
+  }
